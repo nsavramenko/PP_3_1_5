0992b8d0dfbdbf53c9ac700f1a82e5bbf56f2ec8
nsavramenko <navramenko.work@gmail.com>
2023-04-02 04:02:01 -0200

    1.1

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -29,7 +29,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/login").permitAll()
                 .antMatchers("/user/").hasAnyRole("ADMIN", "USER")
-                .antMatchers("/admin/").hasRole("ADMIN")
+                .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
                     .and()
                 .formLogin()
